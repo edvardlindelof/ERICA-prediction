e39200220821269c5111543d899d8fad89b397e1
Edvard <edvardlindelof@gmail.com>
2017-07-06 13:14:12 +0200

    bunch of state variables added to NALStateKeeper

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # ERICA-prediction
-This repository is for experimentation with different models for predicting upcoming workload at an emergency department. It is part of the ERICA-project, a planning tool for emergency departments and part of [Sequence Planner](https://github.com/kristoferB/SP/tree/erica).
+This repository is for experimentation with different models for predicting upcoming workload at an emergency department. It is part of the ERICA-project, a planning tool for emergency departments that mainly resides in [Sequence Planner](https://github.com/kristoferB/SP/tree/erica).
--- dataaggregation/src/main/scala/dataaggregation/LaunchNALStateKeeper.scala
+++ dataaggregation/src/main/scala/dataaggregation/LaunchNALStateKeeper.scala
@@ -16,18 +16,28 @@ object LaunchNALStateKeeper extends App {
 
 class NALStateKeeper extends RecoveredEventHandler {
 
-  val patientsAtNAL = mutable.Set[Int]() // set of CareContactIds
+  val priorities = List("PRIO1", "PRIO2", "PRIO3", "PRIO4", "PRIO5")
+  val teams = List("NAKME", "NAKM", "NAKKI", "NAKOR", "NAKBA", "NAKÖN") // "valid clinics" according to ericaBackend code
+  val invalidTeams = List("NAKIN", "NAK23T", "NAK29", "NAKKK") // TODO should I include these??
+  val patientKinds = List("all", "MEP", "triaged") ::: priorities ::: teams ::: invalidTeams
+  // sets are of CareContactIds
+  val patientSets: Map[String, mutable.Set[Int]] = patientKinds.map(str => str -> mutable.Set[Int]()).toMap
 
   override def handleEvent(ev: EricaEvent) = {
-    println("patients at NAL: " + patientsAtNAL.size)
+    println(ev.Start + "  " + patientKinds.map(kind => kind + ": " + patientSets(kind).size).mkString(", "))
     ev.Category match {
-      case "RemovedPatient" => patientsAtNAL.remove(ev.CareContactId)
-      case "Q" => patientsAtNAL.add(ev.CareContactId)
-      case s: String if s.contains("removed") => patientsAtNAL.remove(ev.CareContactId)
+      case "RemovedPatient" => patientSets.foreach(t => t._2.remove(ev.CareContactId))
+      case "Q" => patientSets("all").add(ev.CareContactId)
+      case "T" => patientSets("all").add(ev.CareContactId)
+      case "P" => patientSets(ev.Type).add(ev.CareContactId)
+      case "ReasonForVisitUpdate" if ev.Value == "MEP" => patientSets("MEP").add(ev.CareContactId)
+      case "TeamUpdate" => patientSets(ev.Value).add(ev.CareContactId)
+      //case s: String if s.contains("removed") => patientsAtNAL.remove(ev.CareContactId)
       case _ => ()
     }
     ev.Type match {
-      case "KLAR" => patientsAtNAL.remove(ev.CareContactId)
+      //case "KLAR" => patientsAtNAL.remove(ev.CareContactId)
+      case "TRIAGE" => patientSets("triaged").add(ev.CareContactId)
       case _ => ()
     }
   }
