1f5a91ff2fd289565719acfca664ad7443d7d3e3
Edvard <edvardlindelof@gmail.com>
2017-07-29 19:27:49 +0200

    QLasso: generation of flow-features

--- models/QLasso/train_and_export_model.py
+++ models/QLasso/train_and_export_model.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import tensorflow as tf
 from tensorflow.contrib import learn
 from tensorflow.contrib.learn import LinearRegressor
@@ -16,7 +18,7 @@ def model(features, targets, mode):
 
     loss = tf.reduce_mean(tf.square(y - targets)) + tf.reduce_sum(tf.abs(W))
     global_step = tf.train.get_global_step()
-    optimizer = tf.train.GradientDescentOptimizer(0.001)
+    optimizer = tf.train.GradientDescentOptimizer(0.1)
     train = tf.group(optimizer.minimize(loss), tf.assign_add(global_step, 1))
 
     return tf.contrib.learn.ModelFnOps(
@@ -24,12 +26,36 @@ def model(features, targets, mode):
         loss=loss,
         train_op=train)
 
-FEATURES = ["ttt30", "all", "MEP", "triaged", "PRIO3", "PRIO4"]
+#FEATURES = ["ttt30", "all", "MEP", "triaged", "PRIO3", "PRIO4"]
+WAIT_TIME_FEATURES = ["ttt30", "ttl30", "ttk30", "ttt60", "ttl60", "ttk60", "ttt120", "ttl120", "ttk120"]
+# to be picky one should calculate e.g. untriaged = all - triaged, but regression weights can be negative so leaving it for now
+WORKLOAD_FEATURES = ["UntreatedLowPrio", "all", "MEP", "triaged", "metdoctor", "done", "PRIO1", "PRIO2", "PRIO3", "PRIO4", "PRIO5"]
+CAPACITY_FEATURES = ["doctors60", "teams60"]
+
+FEATURES = WAIT_TIME_FEATURES + WORKLOAD_FEATURES + CAPACITY_FEATURES
 
 pdframe = pd.read_csv("QLasso2017-07-28T11:36:47.045+02:00.csv")
 
+def generate_Q_features(frame, workload_features, capacity_features):
+    Q_features = {}
+    for workload in workload_features:
+        for capacity in capacity_features:
+            if capacity == 0: # pretty arbitrary constant 0.5 to avoid division by 0
+                Q_features[workload + "/" + capacity] = frame[workload].get_values() / 0.5
+            else:
+                Q_features[workload + "/" + capacity] = frame[workload].get_values() / frame[capacity].get_values()
+    return Q_features
+
 def input_fn_train():
-    feature_cols = {name: tf.constant(pdframe[name].get_values() / max(pdframe[name].get_values()), dtype=tf.float64) for name in FEATURES}
+    #feature_cols = {name: tf.constant(pdframe[name].get_values() / max(pdframe[name].get_values()), dtype=tf.float64) for name in FEATURES}
+    feature_cols = {}
+    Q_features = generate_Q_features(pdframe, WORKLOAD_FEATURES, CAPACITY_FEATURES)
+    for key in Q_features:
+        col = tf.constant(Q_features[key], dtype=tf.float64)
+        feature_cols[key] = col / tf.reduce_max(col) # normalization
+    for feature in WAIT_TIME_FEATURES:
+        col = tf.constant(pdframe[feature].get_values(), dtype=tf.float64)
+        feature_cols[feature] = col / tf.reduce_max(col) # normalization
     outputs = tf.constant(pdframe["TTLOfNextPatient"].get_values(), dtype=tf.float64)
     #outputs = outputs / tf.reduce_max(outputs)
     return feature_cols, outputs
