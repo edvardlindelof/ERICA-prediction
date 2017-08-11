0112e9c96f7acee4d24efef5db441b6a1c0b07be
Edvard <edvardlindelof@gmail.com>
2017-08-11 15:59:22 +0200

    QLasso: time-of-week feature computed only once, workload features included directly, some fit-monitoring

--- models/QLasso/time_of_week_feature.py
+++ models/QLasso/time_of_week_feature.py
@@ -5,7 +5,7 @@ import numpy as np
 
 pdframe = pd.read_csv("QLasso2017-08-04T16:02:58.809+02:00.csv")
 epochsecond = np.array(pdframe["epochseconds"].get_values(), dtype=np.int32)
-ttl_next_low_prio_patient = np.array(pdframe["TTLOfNextPatient"].get_values(), dtype=np.float64)
+ttl_next_low_prio_patient = np.array(pdframe["TTLOfNextPatient"].get_values(), dtype=np.float32)
 
 WEEK_IN_SECONDS = 3600 * 24 * 7
 BIN_WIDTH = 600 # in seconds
@@ -17,18 +17,28 @@ for i in range(len(epochsecond)):
     bin_number = second_in_week / BIN_WIDTH
     wait_time_bins[bin_number] = wait_time_bins[bin_number] + [ttl_next_low_prio_patient[i]]
 
-def to_time_of_week_feature(epochsecond):
-    wait_time_mean_bins = tf.constant([np.mean(wait_time_bins[i]) for i in range(N_BINS)], dtype=tf.float64)
+def to_time_of_week_feature_np(epochsecond):
+    wait_time_mean_bins = np.array([np.mean(wait_time_bins[i]) for i in range(N_BINS)], dtype=np.float32)
     second_in_week = epochsecond % WEEK_IN_SECONDS
-    return tf.map_fn(lambda s: wait_time_mean_bins[s / BIN_WIDTH], second_in_week, dtype=tf.float64)
+    return wait_time_mean_bins[second_in_week / BIN_WIDTH]
+
+def to_time_of_week_feature(epochsecond):
+    wait_time_mean_bins = tf.constant([np.mean(wait_time_bins[i]) for i in range(N_BINS)], dtype=tf.float32)
+    second_in_week = epochsecond % WEEK_IN_SECONDS
+    return tf.map_fn(lambda s: wait_time_mean_bins[s / BIN_WIDTH], second_in_week, dtype=tf.float32)
 
 if __name__ == "__main__":
     sess = tf.Session()
 
     pdframe = pd.read_csv("QLasso2017-08-04T16:02:58.809+02:00.csv")
-    epochsecond = tf.constant(pdframe["epochseconds"].get_values(), dtype=tf.int32)
-    ttl_next_low_prio_patient = tf.constant(pdframe["TTLOfNextPatient"].get_values(), dtype=tf.float64)
 
+    epochsecond = tf.constant(pdframe["epochseconds"].get_values(), dtype=tf.int32)
+    ttl_next_low_prio_patient = tf.constant(pdframe["TTLOfNextPatient"].get_values(), dtype=tf.float32)
     pred = to_time_of_week_feature(epochsecond)
     mse_minutes = tf.losses.mean_squared_error(ttl_next_low_prio_patient, pred) / 3600.
     print sess.run(mse_minutes)
+
+    epochsecond_np = np.array(pdframe["epochseconds"].get_values(), dtype=np.int32)
+    ttl_next_low_prio_patient_np = np.array(pdframe["TTLOfNextPatient"].get_values(), dtype=np.float32)
+    pred_np = to_time_of_week_feature_np(epochsecond_np)
+    print np.mean((pred_np - ttl_next_low_prio_patient_np) ** 2) / 3600
--- models/QLasso/train_and_export_model.py
+++ models/QLasso/train_and_export_model.py
@@ -8,28 +8,33 @@ from tensorflow.contrib.learn.python.learn.utils import input_fn_utils
 tf.logging.set_verbosity(tf.logging.INFO)
 
 import pandas as pd
+import numpy as np
 
-from time_of_week_feature import to_time_of_week_feature
+from time_of_week_feature import to_time_of_week_feature, to_time_of_week_feature_np
 
+
+# TODO plain model stuff into own file
 
 def model(features, targets, mode):
-    W = tf.get_variable("W", [1, len(features)], dtype=tf.float64)
-    b = tf.get_variable("b", [1], dtype=tf.float64)
+    W = tf.get_variable("W", [1, len(features)])
+    b = tf.get_variable("b", [1])
     X = [features[key] for key in features]
     y = tf.reshape(tf.matmul(W, X) + b, [-1])
 
-    un_penaltied_loss = tf.reduce_mean(tf.square(y - targets))
-    loss = un_penaltied_loss + 100.0 * tf.norm(W, ord=1) # TODO penalty hyperparameter
+    #un_penaltied_loss = tf.reduce_mean(tf.square(y - targets))
+    un_penaltied_loss = tf.losses.mean_squared_error(targets, y)
+    #loss = un_penaltied_loss + 100.0 * tf.norm(W, ord=1) # TODO penalty hyperparameter
+    loss = un_penaltied_loss
     global_step = tf.train.get_global_step()
-    optimizer = tf.train.GradientDescentOptimizer(0.001)
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
     train = tf.group(optimizer.minimize(loss), tf.assign_add(global_step, 1))
 
     mse_minutes = tf.div(un_penaltied_loss, 3600, name="mse_minutes")
     tf.summary.scalar("mse_in_minutes", mse_minutes)
-    zero = tf.constant(0, dtype=tf.float64)
+    zero = tf.constant(0, dtype=tf.float32)
     #non_zero_weights = tf.not_equal(W, zero)
-    non_zero_weights = tf.greater(tf.abs(W), zero + 0.1)
-    n_non_zero_weights = tf.reduce_sum(tf.cast(non_zero_weights, tf.float64), name="n_non_zero_weights")
+    non_zero_weights = tf.greater(tf.abs(W), zero + 0.3)
+    n_non_zero_weights = tf.reduce_sum(tf.cast(non_zero_weights, tf.float32), name="n_non_zero_weights")
     tf.summary.scalar("non-zero_weights", n_non_zero_weights)
     #for i in range(len(features)):
     #    tf.summary.scalar("W_element" + str(i), W[0, i])
@@ -48,15 +53,20 @@ CAPACITY_FEATURES = ["doctors60", "teams60"]
 
 FEATURES = WAIT_TIME_FEATURES + WORKLOAD_FEATURES + CAPACITY_FEATURES
 
-pdframe = pd.read_csv("QLasso2017-08-08T14:15:00.357+02:00.csv")
+pdframe = pd.read_csv("QLasso2017-08-04T16:02:58.809+02:00.csv")
+
+# doing this outside of input_fn_train bc if placed in there it will be called maaaaaaany times
+epoch_seconds = np.array(pdframe["epochseconds"].get_values(), dtype=np.int32)
+time_of_week_feature = to_time_of_week_feature_np(epoch_seconds)
+time_of_week_feature = time_of_week_feature / np.max(time_of_week_feature)
 
 def generate_Q_features(frame, workload_features, capacity_features):
     Q_features = {}
     for workload in workload_features:
         for capacity in capacity_features:
-            load = tf.constant(frame[workload].get_values(), dtype=tf.float64)
-            cap = tf.constant(frame[capacity].get_values(), dtype=tf.float64)
-            where_cap_small = tf.cast(tf.less(cap, 0.99), tf.float64)
+            load = tf.constant(frame[workload].get_values(), dtype=tf.float32)
+            cap = tf.constant(frame[capacity].get_values(), dtype=tf.float32)
+            where_cap_small = tf.cast(tf.less(cap, 0.99), tf.float32)
             min_bound_cap = cap + 0.5 * where_cap_small * tf.ones_like(cap) # replace 0s with 0.5s
             Q_features[workload + "/" + capacity] = load / min_bound_cap
     return Q_features
@@ -64,31 +74,36 @@ def generate_Q_features(frame, workload_features, capacity_features):
 def input_fn_train():
     feature_cols = {}
 
-    ttl_next_low_prio_patient = tf.constant(pdframe["TTLOfNextPatient"].get_values(), dtype=tf.float64)
+    ttl_next_low_prio_patient = tf.constant(pdframe["TTLOfNextPatient"].get_values(), dtype=tf.float32)
 
     epoch_seconds = tf.constant(pdframe["epochseconds"].get_values(), dtype=tf.int32)
-    # TODO slows down step time with a factor of about 5 despite only needing to be called once..
-    time_of_week_feature = to_time_of_week_feature(epoch_seconds)
-    feature_cols["TimeOfWeekFeature"] = time_of_week_feature / tf.reduce_max(time_of_week_feature)
+    # slows down step time with a factor of about 5 despite only needing to be called once..
+    #time_of_week_feature = to_time_of_week_feature(epoch_seconds)
+    #feature_cols["TimeOfWeekFeature"] = time_of_week_feature / tf.reduce_max(time_of_week_feature)
+    feature_cols["TimeOfWeekFeature"] = tf.constant(time_of_week_feature)
 
-    untreated_low_prio_col = tf.constant(pdframe["UntreatedLowPrio"].get_values(), dtype=tf.float64)
+    untreated_low_prio_col = tf.constant(pdframe["UntreatedLowPrio"].get_values(), dtype=tf.float32)
     feature_cols["UntreatedLowPrio"] = untreated_low_prio_col / tf.reduce_max(untreated_low_prio_col) # normalization
+
+    for workload in WORKLOAD_FEATURES:
+        load = tf.constant(pdframe[workload].get_values(), dtype=tf.float32)
+        feature_cols[workload] = load / tf.reduce_max(load)
+
+    for feature in WAIT_TIME_FEATURES:
+        col = tf.constant(pdframe[feature].get_values(), dtype=tf.float32)
+        feature_cols[feature] = col / tf.reduce_max(col) # normalization
 
     Q_features = generate_Q_features(pdframe, WORKLOAD_FEATURES, CAPACITY_FEATURES)
     for key in Q_features:
         col = Q_features[key]
         feature_cols[key] = col / tf.reduce_max(col) # normalization
 
-    for feature in WAIT_TIME_FEATURES:
-        col = tf.constant(pdframe[feature].get_values(), dtype=tf.float64)
-        feature_cols[feature] = col / tf.reduce_max(col) # normalization
-
     outputs = ttl_next_low_prio_patient
     #outputs = outputs / tf.reduce_max(outputs)
     return feature_cols, outputs
 
 regressor = learn.Estimator(model_fn=model, model_dir="./modeldir")
-print_tensor = learn.monitors.PrintTensor(["n_non_zero_weights", "mse_minutes"])
+print_tensor = learn.monitors.PrintTensor(["n_non_zero_weights", "mse_minutes", "W"])
 regressor.fit(input_fn=input_fn_train, steps=50000, monitors=[print_tensor])
 
 '''
