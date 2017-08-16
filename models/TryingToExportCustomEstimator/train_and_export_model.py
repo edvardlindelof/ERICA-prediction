1c798f9b5aa2f8e2819d89fe0b0dbda0d6fe5c87
Edvard <edvardlindelof@gmail.com>
2017-08-16 19:02:51 +0200

    CustomModelExporting: simple custom linear thing just for testing

--- models/TryingToExportCustomEstimator/train_and_export_model.py
+++ models/TryingToExportCustomEstimator/train_and_export_model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from tensorflow.contrib.learn import LinearRegressor
+from tensorflow.contrib import learn
 from tensorflow.contrib import layers
 from tensorflow.contrib.learn.python.learn.utils import input_fn_utils
 
@@ -21,12 +22,35 @@ def input_fn_test():
     }
     return feature_cols
 
-feature_cols = [layers.real_valued_column(name) for name in FEATURES]
-regressor = LinearRegressor(feature_columns=feature_cols, model_dir="./modeldir")
-regressor.fit(input_fn=input_fn_train, steps=50)
+def model(features, targets, mode):
+    W = tf.get_variable("W", [1, len(features)])
+    b = tf.get_variable("b", [1])
+    X = [features[key] for key in features]
+    X = tf.cast(X, tf.float32)
+    X = tf.reshape(X, [2, 3])
+    y = tf.reshape(tf.matmul(W, X) + b, [-1])
+
+    loss = tf.losses.mean_squared_error(targets, y)
+
+    global_step = tf.train.get_global_step()
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = tf.group(optimizer.minimize(loss), tf.assign_add(global_step, 1))
+
+    return tf.contrib.learn.ModelFnOps(
+        mode=mode,
+        predictions=y,
+        loss=loss,
+        train_op=train
+    )
+
+#feature_cols = [layers.real_valued_column(name) for name in FEATURES]
+#regressor = LinearRegressor(feature_columns=feature_cols, model_dir="./modeldir")
+regressor = learn.Estimator(model_fn=model, model_dir="./modeldir")
+regressor.fit(input_fn=input_fn_train, steps=150)
 pred = regressor.predict(input_fn=input_fn_test)
 print [p for p in pred]
 
+'''
 def serving_input_fn():
     default_inputs = {col.name: tf.placeholder(col.dtype, [None]) for col in feature_cols}
     features = {key: tf.expand_dims(tensor, -1) for key, tensor in default_inputs.items()}
@@ -40,3 +64,4 @@ regressor.export_savedmodel(
    "exportedmodel",
     serving_input_fn
 )
+'''
